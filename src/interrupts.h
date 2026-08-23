95135e2f0df34aac91e2f65896209d371b242a05
Levin-Programmeierer <boehlerlevin@gmail.com>
2026-08-23 17:49:22 +0200

    Clean up old source structure

--- src/interrupts.h
+++
@@ -1,8 +0,0 @@
-#ifndef IO_H
-#define IO_H
-
-void initialise_GDT(void);
-void exception_handler(unsigned int exception, unsigned int error_code);
-void initialise_IDT(void);
-
-#endif
--- src/terminal.h
+++
@@ -1,8 +0,0 @@
-#ifndef TERMINAL_H
-#define TERMINAL_H
-
-void print(const char *text);
-void clear(void);
-void putchar(char c);
-
-#endif
